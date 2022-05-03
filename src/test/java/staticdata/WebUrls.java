77e220c71d0acdb8cff576fdf2a7a6d9a063321a
LucifeRNoob <andrewkremenevskiy@gmail.com>
2022-05-03 21:50:37 +0300

    Monkkee diploma

--- src/test/java/staticdata/WebUrls.java
+++ src/test/java/staticdata/WebUrls.java
@@ -3,5 +3,4 @@ package staticdata;
 public class WebUrls {
 
     public static final String MONKKEE_URL = "https://my.monkkee.com/#/";
-    public static final String MONKKEE_ENTRIES_URL = "https://my.monkkee.com/#/entries";
-}
+    }
